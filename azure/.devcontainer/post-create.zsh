a06c02a80f9030d00307c66f09b6f124bc1644b7
kosalanuwan <kosala.nuwan@gmail.com>
2021-05-16 20:21:20 +0530

    fix: post-create-command permission denied

--- azure/.devcontainer/post-create.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-# If there's a Soltion file, then run `dotnet restore`
-# It's assumed that the Solution file has all the projects added.
-if [ -f .sln ]; then
-    dotnet restore
-fi
--- dotnet/.devcontainer/post-create.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# If there's a Solution file, then run `dotnet restore`
-# It's assumed that the Solution file has all the projects added.
-if [ -f .sln ]; then
-    dotnet restore && .devcontainer/mssql/postCreateCommand.sh 'P@ssw0rd' './bin/Debug/' './.devcontainer/mssql/'
-else
-    .devcontainer/mssql/postCreateCommand.sh 'P@ssw0rd' './bin/Debug/' './.devcontainer/mssql/'
-fi
