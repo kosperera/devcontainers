69e2b587487faf05d65b60d503db2838b822a743
kosalanuwan <kosala.nuwan@gmail.com>
2021-10-03 10:43:36 +0530

    refactor: clean up unwanted dev containers

--- jekyll/.devcontainer/post-create.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# If there's a Gemfile, then run `bundle install`
-# It's assumed that the file has all the plugins added.
-if [ -f ./Gemfile ]; 
-then
-    echo 'Gemfile found.'
-    bundle install
-fi
