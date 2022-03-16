7bf717b2d99c40bc941d374e233e736f58255bfe
kosalanuwan <kosala.nuwan@gmail.com>
2022-03-16 16:56:26 +0530

    feat(node-ts): remove docker compose to direct refer image
    - remove dockerfile
    - remove docker-compose and related props
    - add docker image ref
    - mount workspace
    - add container env values
    - add vs-code recommended extensions
    - add additional node modules
    
    BREAKING CHANGE: now that the features are available we can mix and match dev containers

--- node-ts/.devcontainer/post-create.zsh
+++ node-ts/.devcontainer/post-create.zsh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# Add Global packages if any.
+# yarn global add tslint-to-eslint-config typescript
+
 # If there's a Package.json, then run `yarn install`
 # It's assumed that the file has all the plugins added.
 if [ -f ./package.json ]; 
