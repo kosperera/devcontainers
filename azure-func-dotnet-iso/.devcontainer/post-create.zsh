1253e5afb97bf970914ee5c433d8b26ed90b822e
kosalanuwan <kosala.nuwan@gmail.com>
2022-03-19 15:29:02 +0530

    fix: add azure func related env variables

--- azure-func-dotnet-iso/.devcontainer/post-create.zsh
+++ azure-func-dotnet-iso/.devcontainer/post-create.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
-echo 'Trust the cert on the current platform'
-dotnet dev-certs https --clean
-dotnet dev-certs https -t
+# echo 'Trust the cert on the current platform'
+# dotnet dev-certs https --clean
+# dotnet dev-certs https -t
 
 # If there's a Solution file, then run `dotnet restore`
 # It's assumed that the Solution file has all the projects added.
